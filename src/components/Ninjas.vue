77ae79072a817159e237f090e066146daa8bf57e
Matheus Barros <matheusborn@hotmail.com>
2021-07-12 19:50:04 -0300

    Props

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -15,17 +15,15 @@
 
 <script>
 export default {
+  props: {
+    ninjas: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  },
   data() {
-    return {
-      ninjas: [
-        { name: "Ryu", speciality: "Vue Components", show: false },
-        { name: "Crystal", speciality: "HTML Wizardry", show: false },
-        { name: "Hitoshi", speciality: "Click Events", show: false },
-        { name: "Tango", speciality: "Conditionals", show: false },
-        { name: "Kami", speciality: "Webpack", show: false },
-        { name: "Yoshi", speciality: "Data Diggin", show: false }
-      ]
-    };
+    return {};
   }
 };
 </script>
